e52f6ae88fdf2edbe5fa4993a749e8eeaf30e3fe
DanielShamir93 <daniel.shamir.93@gmail.com>
2022-01-28 14:06:30 +0200

    :bug: Fix deploy problem take 6

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -18,8 +18,7 @@ const auth = async (req, res, next) => {
     req.user = user;
     next();
   } catch (err) {
-    // res.status(401).send({ error: "Please authenticate." });
-    res.send("Please authenticate")
+    res.status(401).send({ error: "Please authenticate." });
   }
 };
 
