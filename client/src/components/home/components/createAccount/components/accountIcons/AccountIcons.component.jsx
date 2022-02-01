4c8ec5149ee4e9f2a4164212ce089e2eb23af23b
DanielShamir93 <daniel.shamir.93@gmail.com>
2022-02-01 15:23:13 +0200

    Add icons from sprite image to AccountIcons component

--- client/src/components/home/components/createAccount/components/accountIcons/AccountIcons.component.jsx
+++ client/src/components/home/components/createAccount/components/accountIcons/AccountIcons.component.jsx
@@ -1,25 +1,39 @@
+import spritesObject from "../../../../../../utils/sprite/sprite";
 import "./accountIcons.styles.scss";
 
 export default function AccountIcons() {
-
-  const iconsStyles = [];
-
-    console.log(iconsStyles)
-
-  const renderIcons = () => {
-    
+  const renderGenericIcons = () => {
+    const { genericIcons } = spritesObject;
     return (
-      iconsStyles.map((iconStyle) => {
-        return <div style={iconStyle}></div>
-      })
-    )
-  }
+      <div className="account-icons-box">
+        <h4 className="account-icons-name">{genericIcons.name}</h4>
+        <div className="account-gallery">
+        {genericIcons.iconsPosition.map((iconPosition) => {
+          return (
+            <figure
+              className="account-icon"
+              key={iconPosition}
+              style={{
+                backgroundImage: `url(${genericIcons.src})`,
+                backgroundPosition: iconPosition,
+                width: genericIcons.width,
+                height: genericIcons.height,
+              }}
+              onClick={() => {}}
+            ></figure>
+          );
+        })}
+      </div>
+      </div>
+      
+      
+    );
+  };
 
   return (
     <div className="Account-icons">
-        <div className="account-icons-box">
-          {renderIcons()}
-        </div>
+      <h3>Account Icon</h3>
+      {renderGenericIcons()}
     </div>
   );
 }
